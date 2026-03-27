aaed0dba3c472d54e7f02d8121b793a6fec16c42
Luiz Akazawa <luizakazawa@outlook.com>
2026-03-27 09:19:34 +0100

    Changed HttpStaticRequest - Ex1

--- ricm.web.GIVEN/impl/httpserver/itf/impl/HttpStaticRequest.java
+++ ricm.web.GIVEN/impl/httpserver/itf/impl/HttpStaticRequest.java
@@ -2,6 +2,10 @@ package httpserver.itf.impl;
 
 import java.io.IOException;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 
 import httpserver.itf.HttpRequest;
 import httpserver.itf.HttpResponse;
@@ -11,13 +15,48 @@ import httpserver.itf.HttpResponse;
  */
 public class HttpStaticRequest extends HttpRequest {
 	static final String DEFAULT_FILE = "index.html";
-	
+
 	public HttpStaticRequest(HttpServer hs, String method, String ressname) throws IOException {
 		super(hs, method, ressname);
 	}
-	
+
 	public void process(HttpResponse resp) throws Exception {
-	// TO COMPLETE
+		String path = m_ressname;
+
+		if (path == null || path.equals("/")) {
+			path = "/" + DEFAULT_FILE;
+		} else if (path.endsWith("/")) {
+			path = path + DEFAULT_FILE;
+		}
+
+		if (path.startsWith("/")) {
+			path = path.substring(1);
+		}
+
+		File file = new File(m_hs.getFolder(), path);
+
+		if (!file.exists() || !file.isFile()) {
+			resp.setReplyError(404, "Not Found");
+			return;
+		}
+
+		resp.setReplyOk();
+		resp.setContentType(HttpRequest.getContentType(file.getName()));
+		resp.setContentLength((int) file.length());
+
+		PrintStream ps = resp.beginBody();
+
+		FileInputStream fis = new FileInputStream(file);
+		try {
+			byte[] buffer = new byte[8192];
+			int n;
+			while ((n = fis.read(buffer)) != -1) {
+				ps.write(buffer, 0, n);
+			}
+			ps.flush();
+		} finally {
+			fis.close();
+		}
 	}
 
 }
